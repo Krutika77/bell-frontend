c866a1d1628e153f9c1e84e63be266b92f07b161
Erica Garand <egarand13@outlook.com>
2024-10-18 10:32:01 -0400

    fix import errors in App

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './App.scss'
 import HomePage from './pages/HomePage/HomePage'
-import Header from "../components/Header";
-import Footer from "../components/Footer";
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
 import ConnectPage from "./pages/ConnectPage/ConnectPage";
 import QuizPage from './pages/QuizPage/QuizPage';
 import NotFoundPage from "./pages/NotFoundPage/NotFoundPage";
