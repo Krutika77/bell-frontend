1252ef6200a3cf3b8f1c15e3fef7467abe106b13
Nimrat Gill <nimratgill@Nimrats-MBP.lan>
2024-10-17 21:06:31 -0700

    merge develop

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './App.scss'
 import HomePage from './pages/HomePage/HomePage'
-import Header from "../components/Header";
-import Footer from "../components/Footer";
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
 import ConnectForm from "./pages/ConnectForm";
 import NotFoundPage from './pages/NotFoundPage/NotFoundPage';
 
@@ -10,11 +10,13 @@ function App() {
 
   return (
     <BrowserRouter>
+    <Header/>
     <Routes>
       <Route path = '/' element = {<HomePage/>} />
       <Route path="/connect" element={<ConnectForm />} />
       <Route path="*" element = {<NotFoundPage/>}/>
     </Routes>
+    <Footer/>
     </BrowserRouter>
   )
 }
