b79106c5cbdf54622ab47d141a4bf6e3c32c9954
F.L <fang@FLs-MacBook-Pro.local>
2024-10-18 10:26:46 -0400

    add social icons in footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,5 +1,9 @@
 import { NavLink } from "react-router-dom";
 import "./Footer.scss";
+import igIcon from "../../assets/logos/insta.svg";
+import fbIcon from "../../assets/logos/fb.svg";
+import liIcon from "../../assets/logos/li.svg";
+import xIcon from "../../assets/logos/x.svg";
 
 function Footer() {
   return (
@@ -13,12 +17,29 @@ function Footer() {
         </a>
         <a href="https://www.bell.ca/Bell-for-Better">Bell For Better</a>
       </section>
+
       <section className="footer__socials">
-        <a href="https://www.facebook.com/BellLetsTalk/">FACEBOOK</a>
-        <a href="https://twitter.com/Bell_LetsTalk">X</a>
-        <a href="https://www.instagram.com/bell_letstalk/">INSTAGRAM</a>
+        <a href="https://www.facebook.com/BellLetsTalk/">
+          <span className="icon-ui">
+            <img src={fbIcon} alt="facebook-icon" className="icon" />
+          </span>
+        </a>
+        <a href="https://twitter.com/Bell_LetsTalk">
+          <span className="icon-ui">
+            <img src={xIcon} alt="x-icon" className="icon" />
+          </span>
+        </a>
+
+        <a href="https://www.instagram.com/bell_letstalk/">
+          <span className="icon-ui">
+            <img src={igIcon} alt="instagram-icon" className="icon" />
+          </span>
+        </a>
+
         <a href="https://www.linkedin.com/showcase/bell-let's-talk/">
-          LINKEDIN
+          <span className="icon-ui">
+            <img src={liIcon} alt="linkedIn-icon" className="icon" />
+          </span>
         </a>
       </section>
     </footer>
