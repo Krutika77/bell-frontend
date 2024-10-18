f57f55c15629dd6d4e857fd603817a736c73b3d9
Nimrat Gill <nimratgill@Nimrats-MBP.lan>
2024-10-17 20:11:37 -0700

    import app.scss

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import './App.css'
+import './App.scss'
 import HomePage from './pages/HomePage/HomePage'
 import Header from "../components/Header";
 import Footer from "../components/Footer";
