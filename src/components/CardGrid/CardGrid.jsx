9a0f3bd82feafe481c3b866dd361764eef2827ed
Krutika77 <40793266+Krutika77@users.noreply.github.com>
2024-10-18 14:23:20 -0400

    quiz element

--- src/components/CardGrid/CardGrid.jsx
+++ src/components/CardGrid/CardGrid.jsx
@@ -15,7 +15,7 @@ const CardGrid = () => {
   useEffect(() => {
     const fetchOrganizations = async () => {
       try {
-        const response = await axios.get("http://localhost:5000/organizations");
+        const response = await axios.get("http://localhost:8080/organizations");
         setOrganizations(response.data);
       } catch (error) {
         console.error("Error fetching data:", error);
